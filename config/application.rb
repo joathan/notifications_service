7268719fd6c9be18323a82f037535bbf9346503e
Joathan Souza <joathanf@gmail.com>
2024-11-16 00:55:03 -0300

    Add the 'lib' directory to the autoload path in the application configuration file

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,8 @@ module NotificationsService
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Load lib files
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
