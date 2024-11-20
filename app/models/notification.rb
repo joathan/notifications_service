e0704a34674cb54afe7a59f923d495ac31772a23
Joathan Souza <joathanf@gmail.com>
2024-11-20 19:55:07 -0300

    Add callback to normalize details before saving notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class Notification < ApplicationRecord
-  validates :user_id, :title, :message, presence: true
+  before_save :normalize_details
+
+  private
+
+  def normalize_details
+    self.details = details.map(&:to_h) if details.is_a?(Array)
+  end
 end
