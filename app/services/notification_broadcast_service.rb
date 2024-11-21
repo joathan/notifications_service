8eabca1cab2ae0723b4f4d8872780c50be0f8028
Joathan Souza <joathanf@gmail.com>
2024-11-21 01:00:59 -0300

    Implement ActionCable for notification broadcasting and update cable configuration

---
+++ app/services/notification_broadcast_service.rb
@@ -0,0 +1,8 @@
+class NotificationBroadcastService
+  def self.broadcast_to_user(user_id, notification)
+    ActionCable.server.broadcast(
+      "notifications_#{user_id}",
+      notification
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  mount ActionCable.server => '/cable'
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 
