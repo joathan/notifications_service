d714e8468f961356920162ffbdd0c45b1e9186bf
Joathan Souza <joathanf@gmail.com>
2024-11-21 12:23:08 -0300

    Add string freeze comment and remove cleanup code in notifications channel

--- app/channels/notification_channel.rb
+++ app/channels/notification_channel.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 class NotificationChannel < ApplicationCable::Channel
   def subscribed
     stream_from "notifications_#{params[:user_id]}"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
   end
 end
