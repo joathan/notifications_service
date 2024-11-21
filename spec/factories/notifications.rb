4a05694f456c17d94f60bf37920ab25a1a0937fe
Joathan Souza <joathanf@gmail.com>
2024-11-21 12:22:18 -0300

    Updates the notification factory to include task_id, details and status

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -2,8 +2,10 @@
 
 FactoryBot.define do
   factory :notification do
-    title { Faker::Lorem.sentence }
-    description { Faker::Lorem.paragraph }
+    task_id { 1 }
     user_id { 1 }
+    title { Faker::Lorem.sentence }
+    details { { label: Faker::Lorem.word, value: Faker::Lorem.word } }
+    status { 'completed' }
   end
 end
